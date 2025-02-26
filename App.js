6eb9d75a2d3c372235e7c8a615fcc2d34a2ed535
carter-0 <carterannandale@gmail.com>
2025-02-26 19:30:17 +0000

    feat: initial commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TrueSheet } from "@lodev09/react-native-true-sheet";
+import { useRef } from 'react';
+import { Button, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const sheet = useRef(null)
+
+  // Present the sheet ✅
+  const present = async () => {
+    await sheet.current?.present()
+    console.log('horray! sheet has been presented 💩')
+  }
+
+  // Dismiss the sheet ✅
+  const dismiss = async () => {
+    await sheet.current?.dismiss()
+    console.log('Bye bye 👋')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button onPress={present} title="Present" />
+      <TrueSheet
+        ref={sheet}
+        sizes={['auto', 'large']}
+        cornerRadius={24}
+        onDismiss={() => {
+            alert(1)
+            console.log('[FROM CALLBACK] Sheet dismissed');
+        }}
+        onDragChange={(dragging) => {
+            console.log('[FROM CALLBACK] Dragging:', dragging);
+        }}
+        onPresent={() => {
+            console.log('[FROM CALLBACK] Presented');
+        }}
+        onSizeChange={(size) => {
+            console.log('[FROM CALLBACK] Size changed:', size);
+        }}
+        dismissible={true}
+        grabber
+      >
+        <Button onPress={dismiss} title="Dismiss" />
+      </TrueSheet>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ README.md
@@ -0,0 +1,7 @@
+# Reproduction of https://github.com/lodev09/react-native-true-sheet/issues/148
+
+Tested on physical iPhone SE (3rd generation). Nothing appears in the `console.log` and no `alert` is presented for any of the event handlers.
+
+## Demo
+
+<video src='demo.mov' width=180/>
